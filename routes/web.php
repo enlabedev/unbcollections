1ae4986042ca77f88943c44c0a82ee069d12a6d7
Enrique Lazo Bello <enlabe@gmail.com>
2024-04-20 17:01:43 -0500

    Blade Flow

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-Route::controller(UserController::class)->group(function () {
-    Route::post('/users', 'store');
-    Route::put('/users/{user}', 'update');
-    Route::delete('/users/{user}', 'delete');
-    Route::get('/users', 'index');
-    Route::get('/users/{user}', 'show');
+Route::get('/', function () {
+    return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
