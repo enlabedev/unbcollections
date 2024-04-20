5d485196a621cd4bc0901f6112d965542829c373
Enrique Lazo Bello <enlabe@gmail.com>
2024-04-20 14:17:54 -0500

    Add test: user cannot be created

--- tests/Unit/ClientTest.php
+++ tests/Unit/ClientTest.php
@@ -22,4 +22,16 @@ class ClientTest extends TestCase
         $response->assertStatus(201);
         $this->assertDatabaseHas('users', ['email' => 'info@enlabedev.com']);
     }
+
+    public function test_user_cannot_be_created(): void
+    {
+        $response = $this->post('/users', [
+            'name' => 'Enrique',
+            'lastname' => 'Lazo',
+            'email' => 'info1enlabedev.com',
+            'password' => '12345678',
+            'phone' => '+56912345678']
+        );
+        $response->assertStatus(302);
+    }
 }
